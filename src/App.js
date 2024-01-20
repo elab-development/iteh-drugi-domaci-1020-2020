984c85be4a3c3d39af258649d6e329ed7d3aa6c2
Luka687 <lb20201020@student.fon.bg.ac.rs>
2024-01-20 22:59:27 +0100

    Added cart functionalities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navBar';
+import Books from './components/books';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Cart from './components/cart';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Contact from './components/contact';
 function App() {
+
+    var viewed_book = null;
+
+    // Remove an item from cart
+    const removeFromCart = (id) => {
+      books.map((book) => {
+        if (book.id === id) {
+          if (book.amount > 0) {   
+            book.amount = book.amount - 1;
+            const a = cartNum - 1;
+            setCartNum(a);
+            refreshCart();
+            console.log("Book ID =", book.id, "Amount =", book.amount);
+          } else {
+            alert("There are no copies of this book in the cart!");
+          }
+        }
+      });
+    };
+  
+    const purchase = () => {
+      books.map((book) => {
+          book.amount = 0;
+      })
+      const a = 0;
+      setCartNum(a);
+      refreshCart();
+      alert("Your purchase has been completed!");
+    };
+  
+    // Add an item to cart
+    const addToCart = (id) => {
+      books.map((book) => {
+        if (book.id === id) {
+          book.amount = book.amount + 1;
+          const a = cartNum + 1;
+          setCartNum(a);
+          refreshCart();
+          console.log("Book ID =", book.id, "Amount =", book.amount);
+        }
+      });
+    };
+  
+    // Refresh cart function
+    const refreshCart = () => {
+      const newBooks = books.filter((book) => book.amount > 0);
+      setCartBooks(newBooks);
+    }
+  
+    // Objects (books, number of cart items, books in cart...)
+    const [cartNum, setCartNum] = useState(0);
+    const [books, setBooks] = useState(null);
+    const [cartBooks, setCartBooks] = useState([]);
 
   //Read data from json file
   useEffect(()=>{
