975f6b491d66997c89fe267099fcf72507f59830
Luka Basic <lb20201020@student.fon.bg.ac.rs>
2024-02-09 09:11:28 -0800

    Included middleware in api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserBookController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\CartBookController;
 use App\Http\Controllers\API\AuthController;
 
 
@@ -19,15 +20,15 @@ use App\Http\Controllers\API\AuthController;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
 Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
 
-Route::resource('/books', BookController::class);
+Route::resource('/books', BookController::class)->only(['index','show']);
 Route::resource('users.books', UserBookController::class)->only(['index']);
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -37,8 +38,14 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function(Request $request) {
         return auth()->user();
     });
+    Route::group(['middleware' => ['auth:sanctum', 'CheckRole']], function(){
+        Route::resource('/books', BookController::class)->only(['update','store','destroy']);
+        Route::get('/users/{id}', [UserController::class, 'show']);
+    });
 
-    Route::resource('/books', BookController::class)->only(['update','store','destroy']);
+    Route::group(['middleware' => ['auth:sanctum', 'CheckUser']], function(){
+        Route::resource('/cart', CartBookController::class)->only(['index','update','store','destroy','show']);
+    });
 
     // API route for logout user
     Route::post('/logout', [AuthController::class, 'logout']);
