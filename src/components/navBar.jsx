e7608f8a17b0c19957bcc3da618ac93b83aae5de
Luka687 <lb20201020@student.fon.bg.ac.rs>
2024-02-09 09:43:46 -0800

    Added logout option & minor updates to navBar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,19 +2,35 @@ import React from 'react';
 import { ImCart } from "react-icons/im";
 import { Link } from "react-router-dom";
 
-function NavBar({cartNum}){
-    return(
-        <div className='navBar'>
-            <Link to = "/"><h2>Book Catalogue</h2></Link>   
-            <Link to = "/contact" style={{ marginLeft: 20 }}><h2>Contact</h2></Link>  
-            <Link to = "/cart" className="cart-items">
-                <ImCart style={{ marginLeft: 10}} />
-                <div className="cart-num">{cartNum}</div>
-            </Link>
-            <Link to = "/register"><h2 style={{ marginLeft: 10}}>Register</h2></Link>
-            <Link to = "/login"><h2 style={{ marginLeft: 10}}>Login</h2></Link>            
-        </div>
-    )
+function NavBar({cartNum, isLoggedIn, logout}){
+    if(isLoggedIn === false){
+        return(
+            <div className='navBar'>
+                <Link to = "/"><h2>Book Catalogue</h2></Link>   
+                <Link to = "/contact" style={{ marginLeft: 20 }}><h2>Contact</h2></Link>  
+                <Link to = "/cart" className="cart-items">
+                    <ImCart style={{ marginLeft: 10}} />
+                    <div className="cart-num">{cartNum}</div>
+                </Link>
+                <Link to = "/register"><h2 style={{ marginLeft: 10}}>Register</h2></Link>
+                <Link to = "/login"><h2 style={{ marginLeft: 10}}>Login</h2></Link>            
+            </div>
+        )
+    }
+    else{
+        return(
+            <div className='navBar'>
+                <Link to = "/"><h2>Book Catalogue</h2></Link>   
+                <Link to = "/contact" style={{ marginLeft: 20 }}><h2>Contact</h2></Link>  
+                <Link to = "/cart" className="cart-items">
+                    <ImCart style={{ marginLeft: 10}} />
+                    <div className="cart-num">{cartNum}</div>
+                </Link>  
+                <Link><h2 style={{ marginLeft: 10}} onClick={logout}>Logout</h2></Link>       
+            </div>
+        )
+    }
+    
 }
 
 export default NavBar;
