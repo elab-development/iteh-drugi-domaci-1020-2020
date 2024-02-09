54c853aa060d7ab383ab7250bd2bc4504b5aa764
Luka687 <lb20201020@student.fon.bg.ac.rs>
2024-02-09 09:24:08 -0800

    Connected login component to backend

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,57 +3,53 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect } from "react";
 import axios from "axios";
 
-const Login = () => {
+const Login = ({axios_instance, addToken}) => {
 
-    const [submitted, setSubmitted] = useState(false);
+    const instance = axios_instance;
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
     const navigate = useNavigate();
-    const [users, setUsers] = useState(null);
 
-    useEffect(()=>{
-        const fetchData = async () => {
-          const response = await axios.get('/users.json');
-          setUsers(response.data.users);
-        }
-    
-        fetchData();
-      }, []);
-
-    const handleSubmit = (e) => {  
-      e.preventDefault(); 
-      const email = e.target.email;
-      const password = e.target.password;
-      console.log(email.value);
-
-
-      users.map((user) =>{        
-        if(user.email === email.value && user.password === password.value){
-            setSubmitted(true);
-        }
-        else{
-            alert("Unknown user")
-            setSubmitted(false);
-        }
-      })    
-    };
-  
-  
-    if (submitted) {  
-        alert("User has successfully logged in!");
-        navigate("..\\");
+    const handleEmail = (e) =>{
+      setEmail(e.target.value)
     }
-  
-  
+
+    const handlePassword = (e) =>{
+      setPassword(e.target.value)    
+    }
+       
+    const handleSubmit = () => {
+        instance.post('login',{
+          'email': email,
+          'password': password
+        }).then((res) => {
+            alert("User has successfully logged in!");
+            window.sessionStorage.setItem("auth_token", res.data.token);
+            window.sessionStorage.setItem("auth_name", res.data[0]);
+            window.sessionStorage.setItem("auth_id", res.data[1]);
+            window.sessionStorage.setItem("role_id", res.data[2]);
+            addToken(res.data.token);
+            console.log(window.sessionStorage.getItem("role_id"))
+
+            if(window.sessionStorage.getItem("role_id") === "1"){
+              navigate("..\\admin")
+            }
+            else{
+              navigate("..\\")
+            }
+        }).catch(function(){alert('Login failed, unknown user'); return;});
+      };       
+
     return (  
       <form 
-        onSubmit={handleSubmit}
+        onSubmit={function(){return false}}
       >
-  
         <div>
-          <input type="email" placeholder="Email" name="email" required />
+          <input type="email" placeholder="Email" name="email" required onChange = {handleEmail}/>
           <br></br>
-          <input type="password" placeholder="Password" name="password" required />
+          <input type="password" placeholder="Password" name="password" required onChange = {handlePassword}/>
           <br></br>
-          <button type="submit"> Login </button> 
+          <button type="button" onClick = {handleSubmit}> Login </button> 
         </div>
       </form>
   
